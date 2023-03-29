737513c0400d110b569b03c12bf5f89f70e91a3e
Emely <emelly.dev@gmail.com>
2023-03-29 13:29:38 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/emellyk.png" />
-    <Titulo fontSize={20}>Emelly Kelm</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Emelly Kelm</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        emellyk
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedora Full Stack Python
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
